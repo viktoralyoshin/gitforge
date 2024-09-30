713dd1afda489e1ef3fc4987cb71fef5a896a633
viktoralyoshin <viktor.aleshin2004@gmail.com>
2024-09-30 06:24:43 +0300

    isAuth in bars

--- src/components/ui/dashboard/DashboardSideBar.tsx
+++ src/components/ui/dashboard/DashboardSideBar.tsx
@@ -1,9 +1,19 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { links } from "@/constants";
 import Link from "next/link";
 import { useRouter, usePathname } from "next/navigation";
+import {
+  User,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Avatar,
+  DropdownSection,
+} from "@nextui-org/react";
+import { LuUser2, LuLayoutDashboard, LuSettings, LuBadgeHelp, LuLogOut } from "react-icons/lu";
 
 export default function DashboardSideBar() {
   // const router = useRouter();
@@ -26,25 +36,101 @@ export default function DashboardSideBar() {
           </div>
           <nav className="flex flex-col gap-2">
             {links.map((link) => {
-
-              const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
+              const isActive =
+                (pathname.includes(link.route) && link.route.length > 1) ||
+                pathname === link.route;
 
               return (
                 <Link href={link.route}>
-                  <div className={`${isActive && "bg-foreground-200"} flex items-center gap-2 p-3 hover:bg-foreground-200 rounded-xl ease-in duration-200`}>
+                  <div
+                    className={`${
+                      isActive && "bg-foreground-200"
+                    } flex items-center gap-2 p-3 hover:bg-foreground-200 rounded-xl ease-in duration-200`}
+                  >
                     <Image
                       src={link.image}
                       alt={link.label}
                       width={20}
                       height={20}
                     />
-                    <h4 className="max-lg:hidden font-semibold text-[14px]">{link.label}</h4>
+                    <h4 className="max-lg:hidden font-semibold text-[14px]">
+                      {link.label}
+                    </h4>
                   </div>
                 </Link>
               );
             })}
           </nav>
         </div>
+        <Dropdown backdrop="blur">
+          <DropdownTrigger>
+            <div>
+              <User
+                name="Viktor Alyoshin"
+                description="@viktoralyoshin"
+                className="max-lg:hidden"
+                classNames={{
+                  name: "text-default-600",
+                  description: "text-primary",
+                }}
+                avatarProps={{
+                  size: "sm",
+                  src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                }}
+              />
+              <Avatar
+                className="lg:hidden"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+              />
+            </div>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem isReadOnly key="profile" className="h-14 gap-2">
+              <User
+                name="Viktor Alyoshin"
+                description="@viktoralyoshin"
+                classNames={{
+                  name: "text-default-600",
+                  description: "text-primary",
+                }}
+                avatarProps={{
+                  size: "sm",
+                  src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                }}
+              />
+            </DropdownItem>
+            <DropdownSection title="Profile" showDivider>
+              <DropdownItem key="profile" startContent={<LuUser2 size={18} />}>
+                Profile
+              </DropdownItem>
+              <DropdownItem
+                key="Dashboard"
+                href="/dashboard"
+                startContent={<LuLayoutDashboard size={18} />}
+              >
+                Dashbord
+              </DropdownItem>
+              <DropdownItem
+                key="settings"
+                startContent={<LuSettings size={18} />}
+              >
+                Settings
+              </DropdownItem>
+              <DropdownItem key="help" startContent={<LuBadgeHelp size={18} />}>
+                Help
+              </DropdownItem>
+            </DropdownSection>
+            <DropdownSection>
+              <DropdownItem
+                key="logout"
+                color="danger"
+                startContent={<LuLogOut size={18} />}
+              >
+                Log Out
+              </DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     </div>
   );
--- src/components/ui/main/MainNavbar.tsx
+++ src/components/ui/main/MainNavbar.tsx
@@ -1,8 +1,30 @@
-import { Button, Link } from "@nextui-org/react";
+"use client";
+
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  Link,
+  User,
+} from "@nextui-org/react";
 import { FaArrowRightToBracket } from "react-icons/fa6";
+import {
+  LuUser2,
+  LuLayoutDashboard,
+  LuSettings,
+  LuBadgeHelp,
+  LuLogOut,
+} from "react-icons/lu";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function MainNavbar() {
+  const [isAuth, setAuth] = useState(true);
+
   return (
     <div className="w-full sticky top-0 bg-background z-[100]">
       <div className="flex max-w-7xl justify-between items-center mx-auto my-6 px-6">
@@ -15,10 +37,93 @@ export default function MainNavbar() {
           />
           <h1 className="font-extrabold text-[20px]">GitForge</h1>
         </div>
-        <Button variant="light" as={Link} href="/dashboard" className="font-medium text-[16px] flex gap-2 items-center">
-          Войти
-          <FaArrowRightToBracket />
-        </Button>
+        {isAuth ? (
+          <Dropdown backdrop="blur">
+            <DropdownTrigger>
+              <div>
+                <User
+                  name="Viktor Alyoshin"
+                  description="@viktoralyoshin"
+                  className="max-md:hidden"
+                  classNames={{
+                    name: "text-default-600",
+                    description: "text-primary",
+                  }}
+                  avatarProps={{
+                    size: "sm",
+                    src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                  }}
+                />
+                <Avatar
+                  className="md:hidden"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                />
+              </div>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem isReadOnly key="profile" className="h-14 gap-2">
+                <User
+                  name="Viktor Alyoshin"
+                  description="@viktoralyoshin"
+                  classNames={{
+                    name: "text-default-600",
+                    description: "text-primary",
+                  }}
+                  avatarProps={{
+                    size: "sm",
+                    src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                  }}
+                />
+              </DropdownItem>
+              <DropdownSection title="Profile" showDivider>
+                <DropdownItem
+                  key="profile"
+                  startContent={<LuUser2 size={18} />}
+                >
+                  Profile
+                </DropdownItem>
+                <DropdownItem
+                  key="Dashboard"
+                  href="/dashboard"
+                  startContent={<LuLayoutDashboard size={18} />}
+                >
+                  Dashbord
+                </DropdownItem>
+                <DropdownItem
+                  key="settings"
+                  startContent={<LuSettings size={18} />}
+                >
+                  Settings
+                </DropdownItem>
+                <DropdownItem
+                  key="help"
+                  startContent={<LuBadgeHelp size={18} />}
+                >
+                  Help
+                </DropdownItem>
+              </DropdownSection>
+              <DropdownSection>
+                <DropdownItem
+                  key="logout"
+                  color="danger"
+                  startContent={<LuLogOut size={18} />}
+                >
+                  Log Out
+                </DropdownItem>
+              </DropdownSection>
+            </DropdownMenu>
+          </Dropdown>
+        ) : (
+          <Button
+            variant="light"
+            as={Link}
+            href="/dashboard"
+            className="font-medium text-[16px] flex gap-2 items-center"
+          >
+            Войти
+            <FaArrowRightToBracket />
+          </Button>
+        )}
       </div>
     </div>
   );
